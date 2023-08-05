5a054b16effb7d58dc04d32f7576fd18b99953b9
rezaLin <rezanej666@gmail.com>
2023-08-05 23:26:00 +0330

    Add getComputerChoice function

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ function randomInt(min ,max)
     return Math.floor(Math.random()*(max-min)+min);
 }
 
+function getComputerChoice()
+{
+    choices=["Rock","Paper","Scissors"];
+    return choices[randomInt(0,choices.length)]
+}
