0d0163477e473a89e93fd5f0539b82e82d92e76e
rezaLin <rezanej666@gmail.com>
2023-08-05 23:47:50 +0330

    Add playRound function and its test and change choices of computer to lowercace
    test will be removed in the next commit .choices was making a bug
    i should made them lowercase or do it with toLowerCase function so
    i made them lowercase

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function randomInt(min ,max)
 
 function getComputerChoice()
 {
-    choices=["Rock","Paper","Scissors"];
+    choices=["rock","paper","scissors"];
     return choices[randomInt(0,choices.length)]
 }
 function playerSelection()
@@ -18,4 +18,41 @@ function playerSelection()
     else{
         //raise error .for now i will consider that input is allways right;
     }
+}
+function playRound(computerChoice,playerChoice)
+{
+    // if player wins return 1 if draw 0 and computer -1
+    if(computerChoice===playerChoice)
+    {
+        return 0;
+    }
+    else if(computerChoice==="rock" && playerChoice==="paper"){
+        return 1;
+    }
+    else if(computerChoice==="rock" && playerChoice==="scissors"){
+        return -1;
+    }
+    else if(computerChoice==="paper" && playerChoice==="rock"){
+        return -1;
+    }
+    else if(computerChoice==="paper" && playerChoice==="scissors"){
+        return 1;
+    }
+    else if(computerChoice==="scissors" && playerChoice==="paper"){
+        return 1;
+    }
+    else if(computerChoice==="scissors" && playerChoice==="rock"){
+        return 1;
+    }
+    else if(computerChoice==="rock" && playerChoice==="scissors"){
+        return -1;
+    }
+}
+
+for (let count = 0; count < 10; count++) {
+    let a=getComputerChoice();
+    let b=getComputerChoice();
+    console.log("computer 1: "+a+" computer 2: "+b);
+    console.log("result: "+playRound(a,b));
+    
 }
