55a087101e0ab0ac33647cc1bec3a2e819a7ef6f
rezaLin <rezanej666@gmail.com>
2023-08-05 23:32:40 +0330

    Add playerSelection function

--- script.js
+++ script.js
@@ -8,3 +8,14 @@ function getComputerChoice()
     choices=["Rock","Paper","Scissors"];
     return choices[randomInt(0,choices.length)]
 }
+function playerSelection()
+{
+    let inp=prompt("Enter from(rock,paper,scissors):").toLowerCase();
+    if(inp==="rock"||inp==="paper"||inp==="scissors")
+    {
+        return inp;
+    }
+    else{
+        //raise error .for now i will consider that input is allways right;
+    }
+}
